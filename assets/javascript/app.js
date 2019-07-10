a04d9fedc7214fc53270757795acc9ede44435e9
Greg Manco <gregmanco@gmail.com>
2019-07-10 17:28:58 -0400

    Restarted since old js file wasn't working. Added more content to index.html as placeholders for when javascript will enter it in later

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+// Wait untill page is fully loaded before rendering to user
+
+// Hide elements of the  game so they aren't displayed prior to  game start
+
+// Timer for quiz
+
+// On game start hide the start button , 
+
+// Show Timer
+
+// Show Questiions
+
+
+
+
+
